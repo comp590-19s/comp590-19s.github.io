006ea7da798675cba25e54669b85b95c10df58ef
Kris Jordan <krisjordan@gmail.com>
2018-12-25 00:58:32 -0500

    Update config for gh pages

--- siteConfig.js
+++ siteConfig.js
@@ -21,15 +21,15 @@ const users = [
 const siteConfig = {
   title: 'Programming Systems - Spring 2018',
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://comp590-19s.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  // url: 'https://comp590-19s.github.io',
+  // baseUrl: '/course-site/',
 
   // Used for publishing and more
-  projectName: 'course-site',
-  organizationName: 'unc-cs',
+  projectName: 'comp590-19s.github.io',
+  organizationName: 'comp590-19s',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
